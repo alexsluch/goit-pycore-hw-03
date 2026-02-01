fcf3ec7afa98008ea02cfc2c79ea75b6c0bf4279
oleksander.sluch <alexander.sluch@gmail.com>
2026-02-01 13:01:51 +0200

    HM3

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # --- TASK 1 ---
+from datetime import timedelta
+
+
 def get_days_from_today(date_string):
 
     from datetime import date, datetime
@@ -76,19 +79,37 @@ def get_upcoming_birthdays(users):
     from datetime import datetime
 
     today = datetime.today().date()
-    today_in_next_year = today.replace(year=today.year + 1)
 
-    print(today,today.weekday())
+    result = []
 
     for user in users:
-        name = user.get("name")
-        birthday = datetime.strptime(user.get("birthday"), "%Y.%m.%d").date()
 
-        print(birthday,birthday.day)
+        birthday = datetime.strptime(user.get("birthday"), "%Y.%m.%d").date()
+        birthday_this_year = birthday.replace(year=today.year)
+
+        delta_days = (birthday_this_year - today).days
+
+        if 0 <= delta_days <= 7:
+
+            congratulation_date = birthday_this_year
+
+            match congratulation_date.weekday():
+                case 5:
+                    congratulation_date += timedelta(days=2)
+                case 6:
+                    congratulation_date += timedelta(days=1)
+
+
+            result.append({
+                "name": user.get("name"),
+                "congratulation_date": congratulation_date.strftime("%Y.%m.%d")
+            })
+
+    return result
 
 
 users = [
-    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "John Doe", "birthday": "1985.02.01"},
     {"name": "Jane Smith", "birthday": "1990.01.27"}
 ]
 upcoming_birthdays = get_upcoming_birthdays(users)
